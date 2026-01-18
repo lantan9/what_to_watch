833b3606eba89132f5f6f7015385773584b7f3fb
Lada Antonova <ladans9@yandex.ru>
2026-01-19 01:06:42 +0300

    until validation

---
+++ opinions_app/api_views.py
@@ -0,0 +1,56 @@
+from random import randrange
+
+from flask import jsonify, request
+
+from . import app, db
+from .models import Opinion
+from .views import random_opinion
+
+
+
+@app.route('/api/opinions/<int:id>/', methods=['GET'])
+def get_opinion(id):
+    opinion = Opinion.query.get_or_404(id)
+    return jsonify({'opinion': opinion.to_dict()}), 200
+
+
+@app.route('/api/opinions/<int:id>/', methods=['PATCH'])
+def update_opinion(id):
+    data = request.get_json()
+    opinion = Opinion.query.get_or_404(id)
+    opinion.title = data.get('title', opinion.title)
+    opinion.text = data.get('text', opinion.text)
+    opinion.source = data.get('source', opinion.source)
+    opinion.added_by = data.get('added_by', opinion.added_by)
+    db.session.commit()
+    return jsonify({'opinion': opinion.to_dict()}), 200
+
+
+@app.route('/api/opinions/<int:id>/', methods=['DELETE'])
+def delete_opinion(id):
+    opinion = Opinion.query.get_or_404(id)
+    db.session.delete(opinion)
+    db.session.commit()
+    return '', 204
+
+
+@app.route('/api/opinions/', methods=['GET'])
+def get_opinions():
+    opinions = Opinion.query.all()
+    opinions_list = [opinion.to_dict() for opinion in opinions]
+    return jsonify({'opinions': opinions_list}), 200
+
+
+@app.route('/api/opinions/', methods=['POST'])
+def add_opinion():
+    data = request.get_json()
+    opinion = Opinion()
+    opinion.from_dict(data)
+    db.session.add(opinion)
+    db.session.commit()
+    return jsonify({'opinion': opinion.to_dict()}), 200
+
+@app.route('/api/get-random-opinion/', methods=['GET'])
+def get_random_opinion():
+    opinion = random_opinion()
+    return jsonify({'opinion': opinion.to_dict()}), 200
